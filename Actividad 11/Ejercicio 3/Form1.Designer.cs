296d728fbe21846f1117e55283c4ffc91a81ebfd
= <=>
2025-06-21 13:48:44 -0300

    me habia olvidado de hacer el 3

--- Actividad 11/Ejercicio 3/Form1.Designer.cs
+++ Actividad 11/Ejercicio 3/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Ejercicio_3
 {
-    partial class Form1
+    partial class FormPrincipal
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            label2 = new Label();
+            tbValor = new TextBox();
+            lbResultado = new Label();
+            btnRegistrar = new Button();
+            btnCalcularPromedio = new Button();
+            tbResultado = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(145, 90);
+            label1.Name = "label1";
+            label1.Size = new Size(33, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Valor";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(145, 148);
+            label2.Name = "label2";
+            label2.Size = new Size(59, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Promedio";
+            // 
+            // tbValor
+            // 
+            tbValor.Location = new Point(189, 87);
+            tbValor.Name = "tbValor";
+            tbValor.Size = new Size(100, 23);
+            tbValor.TabIndex = 2;
+            // 
+            // lbResultado
+            // 
+            lbResultado.AutoSize = true;
+            lbResultado.Location = new Point(220, 148);
+            lbResultado.Name = "lbResultado";
+            lbResultado.Size = new Size(69, 15);
+            lbResultado.TabIndex = 3;
+            lbResultado.Text = "lbResultado";
+            // 
+            // btnRegistrar
+            // 
+            btnRegistrar.Location = new Point(341, 87);
+            btnRegistrar.Name = "btnRegistrar";
+            btnRegistrar.Size = new Size(132, 23);
+            btnRegistrar.TabIndex = 4;
+            btnRegistrar.Text = "Registrar";
+            btnRegistrar.UseVisualStyleBackColor = true;
+            btnRegistrar.Click += btnRegistrar_Click;
+            // 
+            // btnCalcularPromedio
+            // 
+            btnCalcularPromedio.Location = new Point(341, 144);
+            btnCalcularPromedio.Name = "btnCalcularPromedio";
+            btnCalcularPromedio.Size = new Size(132, 23);
+            btnCalcularPromedio.TabIndex = 5;
+            btnCalcularPromedio.Text = "Calcular Promedio";
+            btnCalcularPromedio.UseVisualStyleBackColor = true;
+            btnCalcularPromedio.Click += btnCalcularPromedio_Click;
+            // 
+            // tbResultado
+            // 
+            tbResultado.Location = new Point(145, 200);
+            tbResultado.Multiline = true;
+            tbResultado.Name = "tbResultado";
+            tbResultado.Size = new Size(328, 116);
+            tbResultado.TabIndex = 6;
+            // 
+            // FormPrincipal
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(545, 372);
+            Controls.Add(tbResultado);
+            Controls.Add(btnCalcularPromedio);
+            Controls.Add(btnRegistrar);
+            Controls.Add(lbResultado);
+            Controls.Add(tbValor);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "FormPrincipal";
+            Text = "FormPrincipal";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox tbValor;
+        private Label lbResultado;
+        private Button btnRegistrar;
+        private Button btnCalcularPromedio;
+        private TextBox tbResultado;
     }
 }
--- Actividad 11/Ejercicio 3/Form1.cs
+++ Actividad 11/Ejercicio 3/Form1.cs
@@ -1,10 +1,54 @@
+using Microsoft.Win32;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
+using System.Drawing;
+using System.Numerics;
 namespace Ejercicio_3
 {
-    public partial class Form1 : Form
+    //El botón registrar debe guardar el valor de tbValor en un vector de double de 100 elementos
+    //El botón Calcular Promedio debe calcular y mostrar el promedio en lbResultado y tbResultado.
+    //Para eso tiene que recorrer el vector para acumular los valores de este y luego así poder calcular el promedio
+    public partial class FormPrincipal : Form
     {
-        public Form1()
+        int contador = 0;
+        double[] valores = new double[100];
+
+        public FormPrincipal()
         {
             InitializeComponent();
+        }
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+
+                valores[contador] += Convert.ToDouble(tbValor.Text);
+                contador++;
+
+                tbValor.Clear();
+
+        }
+
+        private void btnCalcularPromedio_Click(object sender, EventArgs e)
+        {
+            double acumulador = 0;
+
+            for (int i = 0; i < contador; i++)
+            {
+                acumulador += valores[i];
+            }
+
+            double promedio = 0;
+
+            if (contador > 0)
+            {
+                promedio = acumulador / contador;
+                lbResultado.Text = $"{promedio}";
+                tbResultado.Text = $"{promedio}";
+            }
+            else
+            {
+                lbResultado.Text = "No se han registrado valores.";
+                tbResultado.Clear();
+            }
         }
     }
 }
